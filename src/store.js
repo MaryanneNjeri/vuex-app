95db0a3a2af231b7cc1f43c6ac1bb33105c39cc7
MaryAnne45 <mary.jereh@gmail.com>
2019-01-21 14:16:44 +0300

    intial commit

--- src/store.js
+++ src/store.js
@@ -4,8 +4,14 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  //we define our data here
   state: {
-
+  title: 'My custom title',
+  links: [
+    'http://google.com',
+    'http://coursetro.com',
+    'http://youtube.com'
+  ]
   },
   mutations: {
 
